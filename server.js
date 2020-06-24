82fd33c10777ab11f2070cdc6832b9c7382e3e58
Mohammed Abdul Wasey <abdulwasey13@gmail.com>
2020-06-24 11:58:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log(`Server is starting at port`);
+});
